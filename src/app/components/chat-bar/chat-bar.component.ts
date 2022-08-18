4b20d3865fe470efb1fcbdee7e4a6437efe81d5a
Chinguyenitfe <chinguyenvxvx@gmail.com>
2022-08-18 20:11:01 +0700

    get images file input

--- src/app/components/chat-bar/chat-bar.component.ts
+++ src/app/components/chat-bar/chat-bar.component.ts
@@ -11,6 +11,7 @@ export class ChatBarComponent implements OnInit {
   public message: string = '';
   public isEmojiPickerVisible: boolean | undefined;
   @Output() sendChat = new EventEmitter();
+  public images: any = [];
 
   constructor() {
   }
@@ -29,5 +30,22 @@ export class ChatBarComponent implements OnInit {
 
   public addEmoji(event: any) {
     if (event?.emoji?.native) this.message += event.emoji.native;
+  }
+
+  public chooseFile(event: any) {
+    if (event.target.files) {
+      let reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.images.push({
+          id: this.images.length,
+          url: event.target.result
+        });
+      };
+    };
+  };
+
+  public removeFile(id: any) {
+    this.images = this.images.filter((image: any) => image.id !== id);
   }
 }
